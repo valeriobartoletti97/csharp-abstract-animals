24292167e406d0f49e70473546a33aebd495f263
Valerio Bartoletti <valerio.bartoletti97@gmail.com>
2024-04-24 14:06:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace csharp_abstract_animals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
